a2b6978d212ab315e996e77fd392e6a7ecef9074
Adrià Gomis <gomisroca@gmail.com>
2025-06-03 09:34:18 +0200

    http - basic server, serving a static asset, and with a / route that takes a qParam

---
+++ http/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	fmt.Fprintf(w, "Your Token is: %s\n", r.URL.Query().Get("token"))
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
+}
