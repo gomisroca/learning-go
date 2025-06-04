88d8be8477b09a9e42991ebfabcbb38b01f81f58
Adrià Gomis <gomisroca@gmail.com>
2025-06-04 18:07:55 +0200

    basic infra

---
+++ basics/main.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	// Uncomment the function you want to run
+
+	// functions()
+	// vars()
+	// forLoop()
+	// ifElse()
+	// packages()
+	// switches()
+	// defers()
+	// pointers()
+	// structs()
+	// arrays()
+	// maps()
+	// methods()
+	interfaces()
+}
