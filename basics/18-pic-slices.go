dd21d10330dfcd215b7047c2f8b89b81c1f5af33
Adrià Gomis <gomisroca@gmail.com>
2025-06-08 16:00:29 +0200

    a few more comments about make and nested slices

--- basics/18-pic-slices.go
+++ basics/18-pic-slices.go
@@ -3,13 +3,15 @@ package main
 import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
-	// Create a slice of slices that holds a dy number of rows, each one being a []uint8
-	// Each element of the slice represents a pixel intensity (0 is black, 255 is white)
+	// Create a slice of slices, specifically, a slice of []uint8 with dy length
+	// This means we have a 2D array of dy rows, we have the Y axis of the image
+	// Each element of this outer slice is initially nil, meaning we don't have the X axis of the image yet
 	img := make([][]uint8, dy)
 	
 	// Loop over each row, top to bottom since we are going over dy
 	// The outer loops controls which row we are filling
 	for y := 0; y < dy; y++ {
+		// Now we create the X axis for each row
 		row := make([]uint8, dx) // Create a new row of pixels
 		// The inner loop controls which column we are filling
 		// So we go 
@@ -20,6 +22,7 @@ func Pic(dx, dy int) [][]uint8 {
 		for x := 0; x < dx; x++ { // For each column, calculate a number based on x ^ y
 			row[x] = uint8((x ^y)) // Run a function on each element
 		} 
+		// Finally, we say that the []uint8 that should be in this row (Y axis), is this set of columns (X axis)
 		img[y] = row // Add the row to the slice
 	}
 	
