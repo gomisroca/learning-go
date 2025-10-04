7edfbe8e62c02ffcc56fd8b7d67ab9fe0c56d287
Adrià Gomis <gomisroca@gmail.com>
2025-10-04 15:49:28 +0200

    Web Examples - Static Files

---
+++ web-examples/06-assets-and-files/go.mod
@@ -0,0 +1,3 @@
+module github.com/gomisroca/go/web-examples/assets-and-files
+
+go 1.25.1
---
+++ web-examples/06-assets-and-files/main.go
@@ -0,0 +1,12 @@
+package main
+
+// How to serve static files like CSS, JS, images, etc from a specific directory
+
+import "net/http"
+
+func main() {
+	fs := http.FileServer(http.Dir("assets/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":8080", nil)
+}
